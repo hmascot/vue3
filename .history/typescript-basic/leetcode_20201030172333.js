8f613c746d9c4ce63776552cc0bdea1cb7590438
zhehao_zhang <zhangzhehao@ky-tech.com.cn>
2020-10-31 10:53:23 +0800

    first commit

---
+++ .history/typescript-basic/leetcode_20201030172333.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {string}
+ */
+var twoSum = function(nums, target) {
+    for(let i=0; i<nums.length-1; i++){
+        let a;
+        let b;
+        for(let k=i+1; k<nums.length; k++){
+            if(nums[i] + nums[k] === target) {
+                a = nums[i]
+                b = nums[k]
+                return `${a},${b}`
+            }
+        }
+    }
+};
+console.log(twoSum([2,7,11,15],13))
